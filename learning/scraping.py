6c44a5cd85e0da387ebf33576c1a79127d2f34b6
Chams <chams.toure@gmail.com>
2021-02-09 21:31:04 +0100

    implementation of the speech recognition feature

--- learning/scraping.py
+++ learning/scraping.py
@@ -7,14 +7,32 @@ import playsound
 from gtts import gTTS
 
 iteration = 0
+
 def speak(text,num_of_iter):
-    tts = gTTS(text=text, lang="en")
+    tts = gTTS(text=text, lang="de")
     filename = "voice" + str(num_of_iter)+".mp3"
     tts.save(filename)
     playsound.playsound(filename)
 
+#function that listens to the connected microphone and convert the speech into text
+def get_audio():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        audio = r.listen(source)
+        said = ""
+
+        try:
+            said = r.recognize_google(audio, language='de-DE')
+            print(said)
+        except Exception as e:
+            print("Exception: "+ str(e))
+    return said
+
+speak("hallo chams",1)
+the_word = get_audio()
+
 #The website we want to inspect
-res = requests.get("https://www.dictionary.com/browse/psychology")
+res = requests.get("https://www.dictionary.com/browse/" + the_word)
 res.raise_for_status()
 soup = bs4.BeautifulSoup(res.text, "html.parser")
 elements = soup.select("div")
@@ -27,9 +45,18 @@ print(len(elements))
 for item in elements:
     if "value" in item.attrs.keys():
         definitions.append(item.getText())
-        print(item.getText()+ "\n")
+        #print(item.getText()+ "\n")
 
-for i in definitions:
-    speak(i, iteration)
-    iteration +=1
-speak("hello chams")
+#will print the first two definitions if available
+if len(definitions) > 1:
+    print(definitions[0])
+    print(definitions[1])
+else:
+    print(definitions[0])
+#each definition will be spoke by the assistant and saved as a file with the iteration number
+#for i in definitions:
+#    speak(i, iteration)
+#    iteration +=1
+
+
+
